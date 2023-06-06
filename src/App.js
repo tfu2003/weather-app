33d5fef99aaa5865432a3f2c46e4543faafaf4d7
tfu2003 <officialtonyfu@gmail.com>
2023-06-06 13:11:23 -0700

    fixed env variable

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const url =
+    "https://weatherapi-com.p.rapidapi.com/forecast.json?q=Port%20Coquitlam&days=3";
+  const [container, setContainer] = useState([]);
+  const [endpoint, setEndpoint] = useState("");
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": `${process.env.REACT_APP_KEY}`,
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
 
-const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=Port%20Coquitlam&days=3';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': `${process.env.KEY}`,
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-
-fetch(url, options).then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.log(err);
-})
+  fetch(url, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => setContainer(data.forecast.forecastday))
+    .catch((err) => {
+      console.log(err);
+    });
 
   return (
     <div className="App">
-      <div>
-        hi
-      </div>
+      {container.map((element, i) => (
+        // <div>
+        //   {element.date}
+        //   </div>
+        createForecast(element)
+      ))}
     </div>
   );
+}
+
+function createForecast(element) {
+  return (
+    <div>
+      {element.date}
+    </div>
+  )
 }
 
 export default App;
